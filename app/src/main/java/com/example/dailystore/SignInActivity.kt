9eeae5a553c008dbc77138295b8781470e9aec0d
Daniel Boyko <danielboyko02@gmail.com>
2021-11-09 23:17:21 +0200

    Migration to Material Design API

--- app/src/main/java/com/example/dailystore/SignInActivity.kt
+++ app/src/main/java/com/example/dailystore/SignInActivity.kt
@@ -3,35 +3,17 @@ package com.example.dailystore
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.view.View
 
 class SignInActivity : AppCompatActivity() {
-    private lateinit var loginOrEmail: EditText;
-    private lateinit var password: EditText;
-    private lateinit var signInBtn: Button;
-    private lateinit var signUpNav: TextView;
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+    }
 
-        loginOrEmail = findViewById(R.id.loginOrEmail)
-        password = findViewById(R.id.password)
-        signInBtn = findViewById(R.id.signInBtn)
-        signUpNav = findViewById(R.id.signUpNav)
-
-        signInBtn.setOnClickListener {
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-            this.finish()
-        }
-
-        signUpNav.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
-            this.finish()
-        }
+    fun onSignUpNavClick(view: View) {
+        startActivity(Intent(this, SignUpActivity::class.java))
+        overridePendingTransition(R.anim.slide_in_right, R.anim.stay)
+        this.finish()
     }
 }
--- app/src/main/java/com/example/dailystore/SignUpActivity.kt
+++ app/src/main/java/com/example/dailystore/SignUpActivity.kt
@@ -1,68 +1,20 @@
 package com.example.dailystore
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.content.Intent
+
 
 class SignUpActivity : AppCompatActivity() {
-    private lateinit var login: EditText;
-    private lateinit var email: EditText;
-    private lateinit var password: EditText;
-    private lateinit var confirmPassword: EditText;
-    private lateinit var signUpBtn: Button;
-    private lateinit var signInNav: TextView;
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-
-        login = findViewById(R.id.login)
-        email = findViewById(R.id.email)
-        password = findViewById(R.id.password)
-        confirmPassword = findViewById(R.id.confirmPassword)
-        signUpBtn = findViewById(R.id.signUpBtn)
-        signInNav = findViewById(R.id.signInNav)
-
-        signUpBtn.setOnClickListener {
-            checkUserInput()
-        }
-
-        signInNav.setOnClickListener {
-            val intent = Intent(this, SignInActivity::class.java)
-            startActivity(intent)
-            this.finish()
-        }
     }
 
-    private fun checkUserInput() {
-        if (!isLoginValid(login.text.toString())) {
-            Toast.makeText(applicationContext, "Login is invalid", Toast.LENGTH_SHORT).show()
-        }else if (!isEmailValid(email.text.toString())){
-            Toast.makeText(applicationContext, "Email is invalid", Toast.LENGTH_SHORT).show()
-        }else if(password.text.toString() != confirmPassword.text.toString() || !isPasswordValid(password.text.toString())){
-            Toast.makeText(applicationContext, "Password is invalid", Toast.LENGTH_SHORT).show()
-        }
-        else{
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-            this.finish()
-        }
-    }
-
-    private fun isLoginValid(login: String): Boolean {
-        return login.length > 3
-    }
-
-    private fun isEmailValid(email: String): Boolean {
-        return email.length > 3
-    }
-
-    private fun isPasswordValid(password: String): Boolean {
-        return password.length > 3
+    fun onSignInNavClick(view: View) {
+        startActivity(Intent(this, SignInActivity::class.java))
+        overridePendingTransition(R.anim.slide_in_left,android.R.anim.slide_out_right)
+        this.finish()
     }
 }
