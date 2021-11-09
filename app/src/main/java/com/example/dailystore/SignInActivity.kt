f7739a0bf6812a9e2c13b08ff22e2bbe3676f63e
Daniel Boyko <danielboyko02@gmail.com>
2021-11-10 00:45:48 +0200

    Binding View to Model

--- app/src/main/java/com/example/dailystore/SignInActivity.kt
+++ app/src/main/java/com/example/dailystore/SignInActivity.kt
@@ -4,14 +4,60 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import br.com.simplepass.loading_button_lib.customViews.CircularProgressButton
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var usernameOrEmail: EditText
+    private lateinit var password: EditText
+    private lateinit var signInButton: CircularProgressButton
+    private lateinit var restorePassword: TextView
+
+    private lateinit var tvSignUpNav: TextView
+    private lateinit var ivSignUpNav: ImageView
+
+    private lateinit var facebookAuth: ImageView
+    private lateinit var googleAuth: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        usernameOrEmail = findViewById(R.id.editTextUsernameOrEmail)
+        password = findViewById(R.id.editTextPassword)
+        signInButton = findViewById(R.id.buttonSignIn)
+        restorePassword = findViewById(R.id.textViewRestorePassword)
+
+        tvSignUpNav = findViewById(R.id.textViewSignUpNav)
+        ivSignUpNav = findViewById(R.id.imageViewSignUpNav)
+
+        facebookAuth = findViewById(R.id.imageViewFacebookAuth)
+        googleAuth = findViewById(R.id.imageViewGoogleAuth)
+
+        tvSignUpNav.setOnClickListener(signUpNav)
+        ivSignUpNav.setOnClickListener(signUpNav)
+
+        signInButton.setOnClickListener {
+            Toast.makeText(applicationContext, "TODO Sign In", Toast.LENGTH_SHORT).show()
+        }
+
+        restorePassword.setOnClickListener {
+            Toast.makeText(applicationContext, "TODO restore password", Toast.LENGTH_SHORT).show()
+        }
+
+        facebookAuth.setOnClickListener {
+            Toast.makeText(applicationContext, "TODO Facebook Auth", Toast.LENGTH_SHORT).show()
+        }
+
+        googleAuth.setOnClickListener {
+            Toast.makeText(applicationContext, "TODO Google Auth", Toast.LENGTH_SHORT).show()
+        }
     }
 
-    fun onSignUpNavClick(view: View) {
+    private val signUpNav: View.OnClickListener = View.OnClickListener {
         startActivity(Intent(this, SignUpActivity::class.java))
         overridePendingTransition(R.anim.slide_in_right, R.anim.stay)
         this.finish()
--- app/src/main/java/com/example/dailystore/SignUpActivity.kt
+++ app/src/main/java/com/example/dailystore/SignUpActivity.kt
@@ -4,15 +4,60 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.content.Intent
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import br.com.simplepass.loading_button_lib.customViews.CircularProgressButton
 
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var username: EditText
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var confirmPassword: EditText
+    private lateinit var signUpButton: CircularProgressButton
+
+    private lateinit var tvSignInNav: TextView
+    private lateinit var ivSignInNav: ImageView
+
+    private lateinit var facebookAuth: ImageView
+    private lateinit var googleAuth: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        username = findViewById(R.id.editTextUsername)
+        email = findViewById(R.id.editTextEmail)
+        password = findViewById(R.id.editTextPassword)
+        confirmPassword = findViewById(R.id.editTextConfirmPassword)
+        signUpButton = findViewById(R.id.buttonSignUp)
+
+
+        tvSignInNav = findViewById(R.id.textViewSignInNav)
+        ivSignInNav = findViewById(R.id.imageViewSignInNav)
+
+        facebookAuth = findViewById(R.id.imageViewFacebookAuth)
+        googleAuth = findViewById(R.id.imageViewGoogleAuth)
+
+        tvSignInNav.setOnClickListener(signInNav)
+        ivSignInNav.setOnClickListener(signInNav)
+
+        signUpButton.setOnClickListener {
+            Toast.makeText(applicationContext, "TODO Sign Up", Toast.LENGTH_SHORT).show()
+        }
+
+        facebookAuth.setOnClickListener {
+            Toast.makeText(applicationContext, "TODO Facebook Auth", Toast.LENGTH_SHORT).show()
+        }
+
+        googleAuth.setOnClickListener {
+            Toast.makeText(applicationContext, "TODO Google Auth", Toast.LENGTH_SHORT).show()
+        }
     }
 
-    fun onSignInNavClick(view: View) {
+    private val signInNav: View.OnClickListener = View.OnClickListener {
         startActivity(Intent(this, SignInActivity::class.java))
         overridePendingTransition(R.anim.slide_in_left,android.R.anim.slide_out_right)
         this.finish()
